54883597958b45f98f504a67c3854e66a2b5e043
Karina Macancela <karymacancela@gmail.com>
2022-05-14 20:19:50 -0400

    adding nav menu

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,41 @@
 import "./navbar.css"
+import { Link } from 'react-router-dom'
 import Logo from "../../img/logo.svg"
+import Instagram from "../../img/instagram.svg"
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="logo">
-                <a href="/">
-                    <img src={Logo} alt="" />
-                </a>
+            <div className="left">
+                <div className="logo">
+                    <a href="/">
+                        <img src={Logo} alt="" />
+                    </a>
+                </div>
             </div>
-            <div className="nav-menu"></div>
+            <div className="right">
+                <div className="nav-menu">
+                    <div className="nav-item">
+                        <Link to="/">
+                            <img src={Instagram} alt="call" />
+                        </Link>
+                    </div>
+                    <div className="nav-item">
+                        <Link to="/">
+                            <span className="material-symbols-outlined">
+                                android_dialer
+                            </span>
+                        </Link>
+                    </div>
+                    <div className="nav-item">
+                        <Link to="/">
+                            <span className="material-symbols-outlined">
+                                menu
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
