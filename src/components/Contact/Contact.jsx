e4d14dacc20d10eb48d0a1245b46121e6505f8b9
Karina Macancela <karymacancela@gmail.com>
2022-06-05 13:55:39 -0400

    adding react hook form and styled components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import "./contact.css"
+import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom'
+import "./contact.css"
 
 const Contact = ({handleNav}) => {
-
-    const [firstname, handleFirstname] = useState("");
-    const [lastname, handleLastname] = useState("");
-    const [company, handleCompanye] = useState("");
-    const [production, handleProduction] = useState("");
-    const [explain, handleExplain] = useState("");
-
     useEffect(() => {
         handleNav(2);
     });
@@ -19,39 +13,45 @@ const Contact = ({handleNav}) => {
         navigate(to);
     }
 
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div id="contact" className="page">
             <h1 className="title">We’re equally excited to possibly collaborate!</h1>
             <h3>Let us know you a little better! Please fill out the form below and we’ll reach back within 24 hours.</h3>
             <div className="contactForm">
-                <form action="">
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="row">
                         <div className='col'>
                             <label>First Name</label>
-                            <input type="text" name="firstname" />
+                            <input {...register("firstName", { required: true})} />
+                            {errors.firstName && "First name is required"}
                         </div>
                         <div className='col'>
                             <label>Last Name</label>
-                            <input type="text" name="lastname" />
+                            <input {...register("lastName", { required: true})} />
+                            {errors.lastName && "Last name is required"}
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
                             <label>Company Name (optional)</label>
-                            <input type="text" name="company" />
+                            <input {...register("company")} />
                         </div>
                         <div className="col">
-                            <select name="productionsize" id="productionsize">
+                            <select {...register("productionSize", { required: true})}>
                                 <option value="0" selected disabled>Production size</option>
                                 <option value="1">0-50</option>
                                 <option value="2">50-100</option>
                                 <option value="3">100-500</option>
                                 <option value="4">500+</option>
                             </select>
+                            {errors.productionSize && "Production size is required"}
                         </div>
                     </div>
                     <div className="row">
-                        <textarea name="explain" id="" cols="30" rows="10"></textarea>
+                        <textarea {...register("explain")} />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
